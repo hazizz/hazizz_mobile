8eeb7c325d920ee125bf643648ab250d5f2baea5
Kornél <stefankornel02@gmail.com>
2020-07-02 10:46:29 +0200

    Web support added for firebase_core and google_sign_in

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Erik\Projects\apps\hazizz_mobile2"
+export "FLUTTER_ROOT=E:\_ENV\flutter"
+export "FLUTTER_APPLICATION_PATH=G:\Projects\Hazizz\hazizz_mobile"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=E:\_ENV\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.5.5"
 export "FLUTTER_BUILD_NUMBER=72"
 export "DART_OBFUSCATION=false"
