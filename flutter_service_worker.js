6290c05c511eac980218b36ed65421ad70731c5c
skornel02 <stefankornel02@gmail.com>
2020-07-05 08:52:36 +0000

    Deploying to gh-pages from  @ 9f402971c47cc5541e21cbdc4ad52835dcfe8bc7 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "55a296c6acbf80c7a3dece1af06933cc",
+  "main.dart.js": "6dc3ee0c1c20e37f72902ffbf529b4bb",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "eace23e7adc3ebcd49976cceb338b26f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
